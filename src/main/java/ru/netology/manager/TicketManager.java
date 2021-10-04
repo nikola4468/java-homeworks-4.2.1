6df10867fad43800e85b76edae8e68fe45f17c02
nikola4468 <nikola4468@gmail.com>
2021-10-04 16:39:15 +0900

    add comparator

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -7,6 +7,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -45,6 +46,20 @@ public class TicketManager {
             }
         }
         Arrays.sort(result);
+        return result;
+    }
+
+    public Ticket[] searchByFromToAndSortPriceAndTime(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repository.findAll()) {
+            if (ticket.matchesFromTo(from, to)) {
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
         return result;
     }
 }
--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -3,15 +3,18 @@ package ru.netology.manager;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Ticket;
 import ru.netology.repository.NotFoundException;
+import ru.netology.repository.TicketByPriceAscComparator;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TicketManagerTest {
     private TicketManager manager = new TicketManager();
+    private TicketByPriceAscComparator time = new TicketByPriceAscComparator();
+
     Ticket ticket1 = new Ticket(1, 45000, "DMD", "KIX", 720);
     Ticket ticket2 = new Ticket(2, 40000, "DMD", "ONT", 780);
-    Ticket ticket3 = new Ticket(3, 35000, "DMD", "KIX", 600);
+    Ticket ticket3 = new Ticket(3, 34000, "DMD", "KIX", 710);
     Ticket ticket4 = new Ticket(4, 5000, "VKV", "COB", 700);
     Ticket ticket5 = new Ticket(5, 54000, "SHE", "COB", 400);
     Ticket ticket6 = new Ticket(6, 56000, "ONT", "KIX", 680);
@@ -95,5 +98,43 @@ class TicketManagerTest {
         assertThrows(NotFoundException.class, () -> {
             manager.removeById(8);
         });
+    }
+
+    @Test
+    void shouldSearchByFromToAndSortPriceAndTime() {
+        Ticket ticket8 = new Ticket(8, 35000, "DMD", "KIX", 700);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+
+        Ticket[] actual = manager.searchByFromToAndSortPriceAndTime("DMD", "KIX", time);
+        Ticket[] expected = new Ticket[]{ticket8, ticket3, ticket1,ticket7};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSearchByFromToAndSortPriceAndTimeDouble() {
+        Ticket ticket8 = new Ticket(8, 35000, "DMD", "KIX", 700);
+        Ticket ticket9 = new Ticket(9, 35000, "DMD", "KIX", 700);
+
+        manager.add(ticket1);
+        manager.add(ticket2);
+        manager.add(ticket3);
+        manager.add(ticket4);
+        manager.add(ticket5);
+        manager.add(ticket6);
+        manager.add(ticket7);
+        manager.add(ticket8);
+        manager.add(ticket9);
+
+        Ticket[] actual = manager.searchByFromToAndSortPriceAndTime("DMD", "KIX", time);
+        Ticket[] expected = new Ticket[]{ticket8, ticket9, ticket3, ticket1, ticket7};
+        assertArrayEquals(expected, actual);
     }
 }
